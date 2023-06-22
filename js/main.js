9c396f7eba2452a17a3621ea4d64a115e5236aba
ewoigift <2105065@students.kcau.ac.ke>
2023-06-22 14:20:46 +0300

    slider secion

--- js/main.js
+++ js/main.js
@@ -10,3 +10,32 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".vehicle-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
